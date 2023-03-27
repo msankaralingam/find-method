7873607240d0630ac7656dcf5d7f06f8b64b530c
Your Name <smile2sasi95@gmail.com>
2023-03-27 09:53:11 +0530

    add

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const person = [
+ { name: "David Carlson", age: 30 },
+ { name: "John Cena", age: 34 },
+ { name: "Mike Sheridan", age: 25 },
+ { name: "John Carte", age: 50 }
+];
+person.find(function(sasi){
+	if(sasi.age>30){
+		console.log(sasi);
+	}
+});
